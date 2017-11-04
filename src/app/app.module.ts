67a7ac342f6388ee0464383696bd91259aacbf8b
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-04 22:25:55 +0100

    addded article component with voting up and down method to class constructor Article.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ArticleComponent } from './components/article/article.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ArticleComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
